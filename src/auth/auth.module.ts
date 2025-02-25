b77364b0c08f5592f82203771989cf9761cd528e
IntriguedSayan <sayan.mid590@gmail.com>
2025-02-26 02:30:17 +0530

    feat: added authentication for practices to access patients data

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,18 +4,22 @@ import { PrismaModule } from 'src/prisma/prisma.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { BullModule } from '@nestjs/bull';
+import { JwtStrategy } from './jwt.strategy';
+import { JwtAuthGuard } from './jwt-auth.guard';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
     imports: [
         PrismaModule,
+        PassportModule,
         JwtModule.register({
-            secret: process.env.JWT_SECRET || 'YOUR_SECRET_KEY', // use env variables in production
+            secret: process.env.JWT_SECRET,
             signOptions: { expiresIn: '1d' },
         }),
         BullModule.registerQueue({name:"emailQueue"}),
 
     ],
     controllers: [AuthController],
-    providers: [AuthService]
+    providers: [AuthService, JwtStrategy, JwtAuthGuard]
 })
 export class AuthModule { }
--- src/patients/patient.controller.ts
+++ src/patients/patient.controller.ts
@@ -1,22 +1,40 @@
-import { Body, Controller, Get, Param, Post, Req } from "@nestjs/common";
+import { Body, Controller, Get, Param, Post, Req, UseGuards } from "@nestjs/common";
 import { PatientService } from "./patients.service";
 import { CreatePatientDto } from "./dto/createPatientDto.dto";
+import { JwtAuthGuard } from "src/auth/jwt-auth.guard";
 import { Request } from "express";
+interface User {
+    practiceId: string;
+}
+
+export interface CustomRequest extends Request {
+    user: User;
+}
 
 @Controller("/api/v1/patients")
+@UseGuards(JwtAuthGuard)
 
 export class PatientController {
     constructor(private readonly patientService: PatientService) { }
 
-    @Post("/:practiceId")
-    async addPatient(@Param("practiceId") practiceId: string, @Body() patientDto: CreatePatientDto) {
+    @Post()
+    async addPatient(
+        // @Param("practiceId") practiceId: string, @Body() patientDto: CreatePatientDto
+        @Req() req: CustomRequest, @Body() patientDto: CreatePatientDto
+    ) {
+        const practiceId = req?.user.practiceId;
 
         return this.patientService.addPatient(practiceId, patientDto);
 
     }
 
-    @Get("/:practiceId")
-    async getPatients(@Param("practiceId") practiceId: string) {
+    @Get()
+    async getPatients(
+        // @Param("practiceId") practiceId: string
+        @Req() req: CustomRequest
+    ) {
+        const practiceId = req?.user.practiceId;
         return this.patientService.getPatients(practiceId);
     }
+
 }
